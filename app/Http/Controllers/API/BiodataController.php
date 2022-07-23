c3bcf60460d17fdde9c96aaeecebc0db8e0f50ab
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-07-23 21:23:44 +0700

    added request checking for get spesified data

--- app/Http/Controllers/API/BiodataController.php
+++ app/Http/Controllers/API/BiodataController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Biodata;
 use Illuminate\Http\Request;
 
 class BiodataController extends Controller
 {
-    public function all(Request $request)
+    public function getAllSiswa(Request $request)
     {
         $nisn = $request->input('nisn');
         $nama = $request->input('nama');
+
+        if ($nisn) {
+            $biodata = Biodata::where('nisn', $nisn)->with(['kelas'])->first();
+            if ($biodata) {
+                return ResponseFormatter::success(
+                    $biodata,
+                    'Siswa ditemukan'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Siswa tidak ditemukan',
+                    404
+                );
+            }
+        }
     }
 }
