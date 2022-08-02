cab367ad380a0ea2ba34b9f16b3a0c70b1d14adb
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-02 13:16:19 +0700

    created category peraturan migration

---
+++ database/migrations/2022_08_02_054741_create_category_peraturans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_peraturans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_category');
+            $table->enum('tipe_poin', [0, 1])->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_peraturans');
+    }
+};
