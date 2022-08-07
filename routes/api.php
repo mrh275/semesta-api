6150dfe6cbcbd652102daafb2645b8d348b8254c
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-07 14:13:15 +0700

    added route for register

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\API\BiodataController;
-use App\Http\Controllers\API\KelasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\KelasController;
+use App\Http\Controllers\API\BiodataController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/register', [UserController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
