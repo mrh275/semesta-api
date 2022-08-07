49d7841226283ce233d95854f10dd2c57cba20a2
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-07 15:11:02 +0700

    created method for login user

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Helpers\ResponseFormatter;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
 use Laravel\Fortify\Rules\Password;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -44,6 +45,47 @@ class UserController extends Controller
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
                 'error' => $error,
+            ], 'Authentication failed', 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+
+            $request->validate([
+                'username' => 'required',
+                'password' => ['required']
+            ]);
+
+            $credentials = request(['username', 'password']);
+            // dd($credentials);
+
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized',
+                ], 'Authentication failed', 500);
+            }
+
+            $user = User::where('username', $request->username)->first();
+
+
+
+            if (!Hash::check($request->password, $user->password)) {
+                throw new \Exception('Invalid credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'User logged in successfully');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
             ], 'Authentication failed', 500);
         }
     }
