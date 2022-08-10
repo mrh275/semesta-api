62b0bcd01dec23932d5696324a7ca60672ed70c4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-10 22:15:42 +0700

    created method for add category peraturan

--- app/Http/Controllers/API/PeraturanController.php
+++ app/Http/Controllers/API/PeraturanController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\CategoryPeraturan;
+use Exception;
 use Illuminate\Http\Request;
 
 class PeraturanController extends Controller
 {
-    //
+    public function addCategoryPeraturan(Request $request)
+    {
+        $request->validate([
+            'nama_category' => 'required',
+        ]);
+
+        CategoryPeraturan::create([
+            'nama_category' => $request->nama_category,
+            'tipe_poin' => ($request->tipe_poin) ? $request->tipe_poin : '0'
+        ]);
+
+        $result = CategoryPeraturan::where('nama_category', $request->nama_category)->first();
+
+        return ResponseFormatter::success([
+            'result' => $result
+        ], 'Data added successfully');
+    }
 }
