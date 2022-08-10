4d2b74cf5519e7857561b485abf4e78a9247906f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-10 22:22:07 +0700

    added method for table relation with peraturan table

--- app/Models/CategoryPeraturan.php
+++ app/Models/CategoryPeraturan.php
@@ -10,4 +10,9 @@ class CategoryPeraturan extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function peraturan()
+    {
+        return $this->hasMany(ListPeraturan::class, 'category_peraturan_id', 'id');
+    }
 }
