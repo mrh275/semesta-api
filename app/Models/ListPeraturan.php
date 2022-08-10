a75158be99a23ffa9382f2c8a353dba9181d12b0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-10 22:44:46 +0700

    added guarded field and method relation for peraturan to category peraturan

--- app/Models/ListPeraturan.php
+++ app/Models/ListPeraturan.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\CategoryPeraturan;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ListPeraturan extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function categoryPeraturan()
+    {
+        return $this->belongsTo(CategoryPeraturan::class, 'category_peraturan_id', 'id');
+    }
 }
