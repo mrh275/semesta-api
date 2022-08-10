b4eac106d40998ced5b04532e41628bbfd978d61
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-10 21:39:05 +0700

    created method for relationanl table

--- app/Models/DaftarPelanggaran.php
+++ app/Models/DaftarPelanggaran.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Kelas;
+use App\Models\ListPeraturan;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DaftarPelanggaran extends Model
 {
     use HasFactory;
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class, 'kelas_id', 'id');
+    }
+
+    public function peraturan()
+    {
+        return $this->belongsTo(ListPeraturan::class, 'peraturan_id', 'id');
+    }
+
+    public function category_peraturan()
+    {
+        return $this->belongsTo(CategoryPeraturan::class, 'category_peraturan_id', 'id');
+    }
 }
