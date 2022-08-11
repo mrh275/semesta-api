f63ce1f9175800535dc9e60d8685c088f4475816
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-11 21:56:47 +0700

    created method for add daftar pelanggaran

--- app/Http/Controllers/API/DaftarPelanggaranController.php
+++ app/Http/Controllers/API/DaftarPelanggaranController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\DaftarPelanggaran;
 use Illuminate\Http\Request;
 
 class DaftarPelanggaranController extends Controller
 {
-    //
+    public function addPelanggaran(Request $request)
+    {
+
+        $request->validate([
+            'nisn' => 'required',
+            'kelas_id' => 'required',
+            'peraturan_id' => 'required',
+            'poin' => 'required',
+            'is_negative' => 'required'
+        ]);
+
+        DaftarPelanggaran::create([
+            'nisn' => $request->nisn,
+            'kelas_id' => $request->kelas_id,
+            'peraturan_id' => $request->peraturan_id,
+            'poin' => $request->poin,
+            'is_negative' => ($request->is_negative) ? $request->is_negative : '1'
+        ]);
+
+        $result = DaftarPelanggaran::where('nisn', $request->nisn)->first();
+
+        return ResponseFormatter::success([
+            'result' => $result
+        ], 'Data berhasil ditambahkan');
+    }
 }
