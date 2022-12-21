61f72c2a16e7b87e8d78016d58c1881b0826706e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-21 20:59:00 +0700

    created ekstrakurikuler model

---
+++ app/Models/Ekstrakurikuler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ekstrakurikuler extends Model
+{
+    use HasFactory;
+}
