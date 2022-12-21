824f1115dd249f7a529daf82be8236207837f1f4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-21 11:11:20 +0700

    updated data orang tua fields

--- database/migrations/2022_07_23_111610_create_data_orang_tuas_table.php
+++ database/migrations/2022_07_23_111610_create_data_orang_tuas_table.php
@@ -15,21 +15,28 @@ class CreateDataOrangTuasTable extends Migration
     {
         Schema::create('data_orang_tuas', function (Blueprint $table) {
             $table->id();
-            $table->string('noreg_ppdb')->unique();
+            $table->integer('nis');
             $table->string('nama_ayah');
             $table->string('tempat_lahir_ayah');
             $table->date('tanggal_lahir_ayah');
             $table->string('pendidikan_ayah');
             $table->string('pekerjaan_ayah');
             $table->string('penghasilan_ayah');
-            $table->string('alamat_ayah');
+            $table->text('alamat_ayah');
             $table->string('nama_ibu');
             $table->string('tempat_lahir_ibu');
             $table->date('tanggal_lahir_ibu');
             $table->string('pendidikan_ibu');
             $table->string('pekerjaan_ibu');
             $table->string('penghasilan_ibu');
-            $table->string('alamat_ibu');
+            $table->text('alamat_ibu');
+            $table->string('nama_wali');
+            $table->string('tempat_lahir_wali');
+            $table->date('tanggal_lahir_wali');
+            $table->integer('pendidikan_wali');
+            $table->integer('pekerjaan_wali');
+            $table->integer('penghasilan_wali');
+            $table->text('alamat_wali');
             $table->timestamps();
         });
     }
