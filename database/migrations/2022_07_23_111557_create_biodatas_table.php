d3c4a72b745483cfb915ac1aee38ff7cfc1306f8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-21 21:25:58 +0700

    fixes table fields

--- database/migrations/2022_07_23_111557_create_biodatas_table.php
+++ database/migrations/2022_07_23_111557_create_biodatas_table.php
@@ -14,15 +14,15 @@ class CreateBiodatasTable extends Migration
     public function up()
     {
         Schema::create('biodatas', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->string('nisn')->unique();
             $table->bigInteger('nis')->unique()->nullable();
             $table->bigInteger('nik')->unique();
             $table->string('nama');
             $table->string('jenis_kelamin');
-            $table->int('tingkat');
-            $table->int('rombel_id')->nullable();
-            $table->int('ekskul_id')->nullable();
+            $table->integer('tingkat');
+            $table->integer('rombel_id')->nullable();
+            $table->integer('ekskul_id')->nullable();
             $table->string('tempat_lahir');
             $table->date('tanggal_lahir');
             $table->string('agama');
@@ -40,7 +40,7 @@ class CreateBiodatasTable extends Migration
             $table->integer('kode_pos');
             $table->string('phone');
             $table->string('status_anak');
-            $table->int('anak_ke');
+            $table->integer('anak_ke');
             $table->integer('is_graduated')->default(0);
             $table->timestamps();
         });
