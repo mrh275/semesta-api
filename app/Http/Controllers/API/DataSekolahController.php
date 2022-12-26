a2a520e14a9f68da4a0b3ec4c91e1f42e75bd04e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-26 21:23:50 +0700

    created method for update data sekolah

--- app/Http/Controllers/API/DataSekolahController.php
+++ app/Http/Controllers/API/DataSekolahController.php
@@ -18,4 +18,32 @@ class DataSekolahController extends Controller
             "Berhasil mengambil data"
         );
     }
+
+    public function updateDataSekolah(Request $request)
+    {
+        $data = [
+            'nama_sekolah' => $request->input('nama_sekolah'),
+            'jenjang' => $request->input('jenjang'),
+            'nss' => $request->input('nss'),
+            'npsn' => $request->input('npsn'),
+            'alamat' => $request->input('alamat'),
+            'desa' => $request->input('desa'),
+            'kecamatan' => $request->input('kecamatan'),
+            'kota' => $request->input('kota'),
+            'provinsi' => $request->input('provinsi'),
+            'pos' => $request->input('pos'),
+            'phone' => $request->input('phone'),
+            'email' => $request->input('email'),
+            'website' => $request->input('website'),
+            'headmaster_name' => $request->input('headmaster_name'),
+            'nip_headmaster' => $request->input('nip_headmaster'),
+        ];
+
+        DataSekolah::where('id', 1)->update($data);
+
+        return ResponseFormatter::success(
+            200,
+            'Data berhasil diperbaharui'
+        );
+    }
 }
