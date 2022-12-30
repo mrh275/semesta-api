4b92412dc1c17fe86d20b868a84ae7f82e09d205
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-30 21:32:33 +0700

    modified store method for update data kelas

--- app/Http/Controllers/API/KelasController.php
+++ app/Http/Controllers/API/KelasController.php
@@ -21,11 +21,18 @@ class KelasController extends Controller
 
     public function store(Request $request)
     {
-        $newKelas = $request->input('kelas');
+        $data = [
+            'rombel_type' => $request->input('rombel_type'),
+            'kurikulum_type' => $request->input('kurikulum_type'),
+            'kurikulum_rombel' => $request->input('kurikulum_rombel'),
+            'nama_kelas' => $request->input('nama_kelas'),
+            'tingkat' => $request->input('tingkat'),
+            'jurusan' => $request->input('jurusan'),
+            'wali_kelas_id' => $request->input('wali_kelas_id'),
+            'jumlah_siswa' => $request->input('jumlah_siswa')
+        ];
 
-        $addKelas = Kelas::create([
-            'nama_kelas' => $newKelas
-        ]);
+        $addKelas = Kelas::create($data);
 
         return ResponseFormatter::success(
             $addKelas->all(),
