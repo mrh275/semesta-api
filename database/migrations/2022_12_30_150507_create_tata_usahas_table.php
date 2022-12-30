9e17fba68a523c2205f08ce8d0d8882a5e92d6f4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-12-30 22:06:43 +0700

    created db migration for tata usaha with all fields needed

---
+++ database/migrations/2022_12_30_150507_create_tata_usahas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tata_usahas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_tu');
+            $table->bigInteger('nip')->nullable();
+            $table->bigInteger('nuptk')->nullable();
+            $table->string('gelar_depan');
+            $table->string('gelar_belakang');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->integer('status_ptk');
+            $table->text('alamat');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tata_usahas');
+    }
+};
