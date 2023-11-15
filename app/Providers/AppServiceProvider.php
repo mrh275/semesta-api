a9735a21b1debd4f28a2f6310bd4f5603ae177cf
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-11-15 22:22:19 +0700

    register custom lines code for custom public directory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('path.public', function () {
+            return realpath(base_path() . '/public_html');
+        });
     }
 
     /**
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
+            publicDirectory: 'public_html',
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
